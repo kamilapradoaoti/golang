5a2bdc801957fb50ba166df3500e7bab80bdfb04
kamilapradoaoti <kamilapradoaoti@gmail.com>
2024-07-23 14:02:18 -0400

    melhorias

--- lib/convert.go
+++ lib/convert.go
@@ -1,7 +1,33 @@
 package lib
 
-import "strconv"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
 
 func IntToString(num int) string {
 	return strconv.Itoa(num)
+}
+func ReadJsonFileToStruct(PahtFile string, value any) error {
+	// Lê o arquivo JSON
+	file, err := os.Open(PahtFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Lê o conteudo do arquivo
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	// Descompacta(Unmarshal) os dados do JSON no objeto ModelDesign
+	err = json.Unmarshal(content, value)
+	if err != nil {
+		return err
+	}
+	return nil
 }
